fix SSI calculation and Gravity model

clean_data.py
@@ -39,6 +39,7 @@ for tar in tar_files:
         f = f[f.destino.str.startswith("28")]
         files.append(f)
         df = pd.concat(files)
+    df['visitors'] = np.repeat(1, len(df))
     dfs.append(df.groupby(["origen", "destino"], as_index=False).mean())
 
 df = pd.concat(dfs)
@@ -46,8 +47,8 @@ df = df.groupby(["origen", "destino"], as_index=False).mean()
 df.to_csv("data_madrid_2022_ave.csv", index=False)
 
 
-df = pd.concat(files)
-df.to_csv("data_madrid_2022_ave.csv", index=False)
+df = pd.concat(dfs)
+df.to_csv("data_madrid_2022_monthly_ave.csv", index=False)
 
 # f = files[0]    
 

models.py
@@ -156,7 +156,6 @@ min_val = int(min(madg["28092"]))
 colors = np.array([mpl.cm.get_cmap('gist_heat')(x/len(madg)) for x in range(len(madg))])
 sm = plt.cm.ScalarMappable(cmap=mpl.cm.get_cmap('gist_heat'))
 
-SSI_vis = 2*np.sum(np.minimum(Vij[63], obs[63]))/(np.sum(Vij[63]) + np.sum(obs[63]))
 
 ch_map = mad.plot(figsize=(10,20), color="tan", alpha=0.2, edgecolor="k")
 for c in range(len(madg)):
@@ -170,7 +169,7 @@ for c in range(len(madg)):
     ch_map.plot(x,y, color=colors[c])  
 cbar = plt.colorbar(sm,fraction=0.005, pad=0.001)
 cbar.set_ticklabels([min_val,mid_val,max_val])
-plt.text(0.1, 0.1,"SSI: "+str(round(SSI_vis, 2)), transform=ch_map.transAxes, fontsize=18)
+#plt.text(0.1, 0.1,"SSI: "+str(round(SSI_vis, 2)), transform=ch_map.transAxes, fontsize=18)
 plt.title("Visitation Law Estimate: Average Daily Travellers from Móstoles Jan 2022", fontsize=18)
 plt.axis('off')
 plt.tight_layout()
@@ -252,14 +251,13 @@ for c in range(len(madg)):
     ch_map.plot(x,y, color=colors[c])  
 cbar = plt.colorbar(sm,fraction=0.005, pad=0.001)
 cbar.set_ticklabels([min_val,mid_val,max_val])
-plt.text(0.1, 0.1,"SSI: "+str(round(SSI_rad, 2)), transform=ch_map.transAxes, fontsize=18)
+#plt.text(0.1, 0.1,"SSI: "+str(round(SSI_rad, 2)), transform=ch_map.transAxes, fontsize=18)
 plt.title("Radiation Model Estimate: Average Daily Travellers from Móstoles Jan 2022", fontsize=18)
 plt.axis('off')
 plt.tight_layout()
 plt.savefig("plot_madrid_municipio_radiation.png", dpi=300, bbox_inches='tight', pad_inches=0.1)
 plt.show()
 plt.close()
-
 
 ###############################################################################
 ########################### Gavity Model ######################################
@@ -274,23 +272,20 @@ dj = pd.DataFrame(R, index=mad.ID.unique(), columns=mad.ID.unique())
 dj = dj.T['28092'].values
 T = Tdf_obs.T['28092'].values
 with pm.Model() as mod:
-    t_s = pm.HalfNormal("t_s", 1)
-    theta = pm.HalfNormal("theta", t_s, shape=muni_n)
-    o_s = pm.HalfNormal("o_s", 0.5)
-    omega = pm.HalfNormal("omega", o_s, shape=muni_n)
-    g_s = pm.HalfNormal("g_s", 0.5)
+    theta = pm.HalfNormal("theta", 2)
+    omega = pm.HalfNormal("omega", 2) 
+    g_s = pm.HalfNormal("g_s", 0.25)
     gamma = pm.HalfNormal("gamma", g_s, shape=muni_n)
     lam_den = (Nj**omega)*(dj**-gamma)
     lam_num = at.sum(lam_den)
     lam = pm.Deterministic("lam", theta*Mi*(lam_den/lam_num))
-    alpha = pm.HalfNormal("alpha", 10)
+    alpha = pm.HalfNormal("alpha", 3)
     m = pm.NegativeBinomial("m", mu=lam, alpha=alpha, observed=T)
 
-dag = pm.model_to_graphviz(mod)
-dag.render("gravity_model_dag", format="png")
-dag
+# dag = pm.model_to_graphviz(mod)
+# dag.render("gravity_model_dag", format="png")
+# dag
 
-#sample prior predictive
 with mod:
     ppc = pm.sample_prior_predictive(1000, var_names=['m'])
 ppc = ppc.stack(sample = ['chain', 'draw']).prior_predictive
@@ -306,10 +301,9 @@ plt.legend()
 plt.title("Prior Predictive Checks: Gravity Model")
 plt.savefig("prior_predictive_gravity.png", dpi=300)
 
-#sample MCMC
-with mod:
-    idata = pm.sample(2000, chains=4, cores=12, target_accept=0.99)
 
+with mod:
+    idata = pm.sample(1000, chains=4, cores=12, target_accept=0.99)
 
 pos = idata.stack(sample = ['chain', 'draw']).posterior
 pos_lam = pos['lam'].values.mean(axis=1)
@@ -340,55 +334,11 @@ for c in range(len(madg)):
     ch_map.plot(x,y, color=colors[c])  
 cbar = plt.colorbar(sm,fraction=0.005, pad=0.001)
 cbar.set_ticklabels([min_val,mid_val,max_val])
-plt.text(0.1, 0.1,"SSI: "+str(round(SSI_gra, 2)), transform=ch_map.transAxes, fontsize=18)
+#plt.text(0.1, 0.1,"SSI: "+str(round(SSI_gra, 2)), transform=ch_map.transAxes, fontsize=18)
 plt.title("Gravity Model Estimate: Average Daily Travellers from Móstoles Jan 2022", fontsize=18)
 plt.axis('off')
 plt.tight_layout()
 plt.savefig("plot_madrid_municipio_gravity.png", dpi=300, bbox_inches='tight', pad_inches=0.1)
-plt.show()
-plt.close()
-
-
-#sample posterior predictive
-with mod:
-    ppc = pm.sample_posterior_predictive(idata, var_names=['m'])
-ppc = ppc.stack(sample = ['chain', 'draw']).posterior_predictive
-ppc_m = ppc['m'].values 
-    
-#plot posterior predictive
-pos_lam = ppc_m.mean(axis=1)
-Ndf =  pd.DataFrame({"28092":pos_lam})
-Ndf['ID'] = data.destino.unique()
-madg = pd.merge(mad,Ndf)
-madg = madg.sort_values(by="28092", ascending=False)
-madg.reset_index(inplace=True, drop=True)
-
-max_val = int(max(madg["28092"]))
-mid_val = int(np.median(madg["28092"]))
-min_val = int(min(madg["28092"]))
-
-colors = np.array([mpl.cm.get_cmap('gist_heat')(x/len(madg)) for x in range(len(madg))])
-sm = plt.cm.ScalarMappable(cmap=mpl.cm.get_cmap('gist_heat'))
-
-SSI_gra = 2*np.sum(np.minimum(pos_lam,T))/(np.sum(pos_lam) + np.sum(T))
-
-ch_map = mad.plot(figsize=(10,20), color="tan", alpha=0.2, edgecolor="k")
-for c in range(len(madg)):
-    #ch_map.set_facecolor("grey")
-    muni_dest = madg.ID[c]
-    orig = "28092" 
-    dest_coord = mad[mad.ID==muni_dest].coords.values[0]
-    orig_coord = mad[mad.ID==orig].coords.values[0]
-    x = (dest_coord[0], orig_coord[0])
-    y = (dest_coord[1], orig_coord[1])
-    ch_map.plot(x,y, color=colors[c])  
-cbar = plt.colorbar(sm,fraction=0.005, pad=0.001)
-cbar.set_ticklabels([min_val,mid_val,max_val])
-plt.text(0.1, 0.1,"SSI: "+str(round(SSI_gra, 2)), transform=ch_map.transAxes, fontsize=18)
-plt.title("Gravity Model Predictions: Average Daily Travellers from Móstoles Jan 2022", fontsize=18)
-plt.axis('off')
-plt.tight_layout()
-plt.savefig("plot_madrid_municipio_gravity_ppc.png", dpi=300, bbox_inches='tight', pad_inches=0.1)
 plt.show()
 plt.close()
 
@@ -484,7 +434,7 @@ summ.to_csv("gravity_model_hierarchical_summary.csv")
 
 # #save summary of mcmc posteriors
 # summ = az.summary(idata, hdi_prob=0.9)
-# summ.to_csv("gravity_model_simple_summary.csv")
+# summ.to_csv("gravity_model_hierarchical_summary.csv")
 
 
 ################ Comparison Plot #####################
@@ -510,6 +460,9 @@ vis_means = np.sort(V_vis)
 rad_means = np.sort(V_rad)
 gra_means = np.sort(V_gra)
 
+SSI_vis = 2*np.sum(np.minimum(obs_means, vis_means))/(np.sum(obs_means) + np.sum(vis_means))
+SSI_rad = 2*np.sum(np.minimum(obs_means, rad_means))/(np.sum(obs_means) + np.sum(rad_means))
+SSI_gra = 2*np.sum(np.minimum(obs_means, gra_means))/(np.sum(obs_means) + np.sum(gra_means))
 
 plt.plot(obs_means, obs_means, color='k', linestyle=":", label="Observed")
 plt.scatter(obs_means, vis_means, marker="o", facecolor="w", color="g", label="Visitation model: SSI="+str(round(SSI_vis, 2)))
@@ -525,3 +478,6 @@ plt.savefig("compare_models.png", dpi=300)
 plt.show()
 plt.close()
 
+data_compare = pd.DataFrame({"observed":obs_means, "visitation":vis_means, 
+                             "radiation":rad_means, "gravity":gra_means})
+data_compare.to_csv("data_compare.csv", index=False)